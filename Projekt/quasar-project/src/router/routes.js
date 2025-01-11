bf199da00ced9ed22f0454e1432c00fd80844925
Diego  Pavlovic <diegopavlovic051@gmail.com>
2025-01-11 16:14:38 +0100

    Updejto stranicu3

--- Projekt/quasar-project/src/router/routes.js
+++ Projekt/quasar-project/src/router/routes.js
@@ -5,19 +5,13 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') }
+      component: () => import('pages/ErrorNotFound.vue'),
+      path: '/BiljkaKolokvij',
+      component: () => import('pages/BiljkaKolokvij.vue')
     ]
   },
 
   // Always leave this as last one,
   // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
-    path: '/BiljkaKolokvij',
-    component: () => import('pages/BiljkaKolokvij.vue')
-
-    
-  }
-]
 
 export default routes
