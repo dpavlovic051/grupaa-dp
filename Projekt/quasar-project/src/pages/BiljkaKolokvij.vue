4a3f0856dbe9a5dc9cd1ec301f035145ad18a4f7
Diego Pavlovic <diegopavlovic051@gmail.com>
2025-01-14 14:11:48 +0100

    Dodao1

--- Projekt/quasar-project/src/pages/BiljkaKolokvij.vue
+++ Projekt/quasar-project/src/pages/BiljkaKolokvij.vue
@@ -6,6 +6,6 @@
 
 <script>
 export default {
-  name: 'Biljka'
+  name: "BiljkaKolokvij",
 };
 </script>
--- Projekt/quasar-project/src/pages/IndexPage.vue
+++ Projekt/quasar-project/src/pages/IndexPage.vue
@@ -1,15 +1,13 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <div class="q-pa-md">
+    <h1>Provjera ishoda 1 i 2</h1>
+
     >
-  </q-page>
+  </div>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
